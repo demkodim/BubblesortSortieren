9a9e67d98b38587bd4ca19e1f1219565401ca84c
Dmytro Demko <dmytro.d2@gso.schule.koeln>
2023-06-07 11:45:26 +0200

    Projektdateien hinzufügen.

---
+++ Bubblesort/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel;
+
+int[] Bubblesort = { 45, 10, 15, 4};
+int counter = 0;
+
+for (int write = 0; write < Bubblesort.Length; write++)
+{
+    for (int sort = 0; sort < Bubblesort.Length - 1; sort++)
+    {
+        if (Bubblesort[sort] > Bubblesort[sort + 1])
+        {
+            counter = Bubblesort[sort + 1];
+            Bubblesort[sort + 1] = Bubblesort[sort];
+            Bubblesort[sort] = counter;
+        }
+    }
+    
+}
+for (int i = 0; i < Bubblesort.Length; i++)
+    Console.Write(Bubblesort[i] + " ");
+
+
